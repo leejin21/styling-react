b83b29cdc749176e82390d3d470052f1310b04db
Jin Lee <leejin21c@gmail.com>
2021-01-12 21:06:08 +0900

    css module: classnames

--- src/CSSModule.js
+++ src/CSSModule.js
@@ -1,9 +1,13 @@
 import React from "react";
 import styles from "./CSSModule.module.css";
 
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
 const CSSModule = () => {
     return (
-        <div className={styles.wrapper}>
+        <div className={cx("wrapper", "inverted")}>
             안녕하세요, 저는
             <span className="something"> 리액트입니다.</span>
         </div>
